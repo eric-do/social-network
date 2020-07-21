6c3125d6ff534a320c12d0ba9d9d6969380e25e0
Eric Do <ericdo.617@gmail.com>
2020-07-21 01:01:36 -0700

    Refactor to make promises run in parallel
    There are 6 queries that need to be ran, and none are dependent
    on each other, so I made them run in parallel using Promise.all().

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -1,12 +1,16 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
-const { getTweetsByHandle, userHasCommentedOnTweet,
-        getCommentCountForTweet, userHasFavoritedTweet,
-        getFavoritesCountForTweet, userHasRetweetedTweet,
-        getRetweetCountForTweet } = require('../../database/tweet');
+const {
+  getTweetsByHandle,
+  userHasCommentedOnTweet,
+  getCommentCountForTweet,
+  userHasFavoritedTweet,
+  getFavoritesCountForTweet,
+  userHasRetweetedTweet,
+  getRetweetCountForTweet,
+} = require("../../database/tweet");
 
-
-router.get('/user_timeline', async (req, res) => {
+router.get("/user_timeline", async (req, res) => {
   const { handle } = req.query;
 
   try {
@@ -14,46 +18,57 @@ router.get('/user_timeline', async (req, res) => {
     console.log(tweets);
     res.send(tweets);
   } catch (e) {
-    res.status(400).send()
+    res.status(400).send();
   }
 });
 
-router.get('/:tweet_id/social/:handle', async (req, res) => {
-  // This route returns booleans for whether a user has liked/commented/retweeted a tweet
-  // Query for whether tweet in user's list of likes
-  // Query for like count
-  // Query for whether tweet in user's list of comments
-  // Query for comment count
-  // Query for whether tweet in user's list of retweets
-  // Query for retweet count
-  // Boolean for each as an object
-
+router.get("/:tweet_id/social/:handle", async (req, res) => {
   const { handle, tweet_id } = req.params;
-  const social = {};
-  social.comments = {};
-  social.favorites = {};
-  social.retweets = {};
 
   try {
-    social.comments.active = await userHasCommentedOnTweet(tweet_id, handle);
-    social.comments.count = await getCommentCountForTweet(tweet_id);
-    social.favorites.active = await userHasFavoritedTweet(tweet_id, handle);
-    social.favorites.count = await getFavoritesCountForTweet(tweet_id);
-    social.retweets.active = await userHasRetweetedTweet(tweet_id, handle);
-    social.retweets.count = await getRetweetCountForTweet(tweet_id);
-    // console.log(social);
+    const socialQueries = [
+      userHasCommentedOnTweet(tweet_id, handle),
+      getCommentCountForTweet(tweet_id),
+      userHasFavoritedTweet(tweet_id, handle),
+      getFavoritesCountForTweet(tweet_id),
+      userHasRetweetedTweet(tweet_id, handle),
+      getRetweetCountForTweet(tweet_id),
+    ];
+
+    const [
+      commentsActive,
+      commentsCount,
+      favoritesActive,
+      favoritesCount,
+      retweetsActive,
+      retweetsCount,
+    ] = await Promise.all(socialQueries);
+
+    const social = {
+      comments: {
+        active: commentsActive,
+        count: commentsCount,
+      },
+      favorites: {
+        active: favoritesActive,
+        count: favoritesCount,
+      },
+      retweets: {
+        active: retweetsActive,
+        count: retweetsCount,
+      },
+    };
+
     res.send(social);
   } catch (e) {
     res.status(400).send(e);
   }
-  
 });
 
-router.get('/:tweet_id/comments', async (req, res) => {
+router.get("/:tweet_id/comments", async (req, res) => {
   // This route gets list of comments for each tweet
   // Query comments for a given tweet
   // Send array of comments to client
-  
 });
 
 module.exports = router;
