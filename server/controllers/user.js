3546564ff594448041d44ff24e034eb5d476f27b
Eric Do <ericdo.617@gmail.com>
2020-07-31 00:40:43 -0700

    Make addUser a little more concise

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,30 +1,26 @@
-const models = require('../database');
+const models = require("../database");
 
 const addUser = async (req, res) => {
-  const userByHandle = new models.instance.UsersByHandle({
-    ...req.body, 
-    registration: { 
-      $db_function: 'toTimestamp(now())' 
-    }
-  });
+  const user = {
+    ...req.body,
+    registration: {
+      $db_function: "toTimestamp(now())",
+    },
+  };
 
-  const userByEmail = new models.instance.UsersByEmail({
-    ...req.body, 
-    registration: { 
-      $db_function: 'toTimestamp(now())' 
-    }
-  });
+  const userByHandle = new models.instance.UsersByHandle(user);
+  const userByEmail = new models.instance.UsersByEmail(user);
 
   try {
     await userByHandle.saveAsync();
     await userByEmail.saveAsync();
-    res.status(201).send({message: "User successfully created"});
-    } catch (e) {
+    res.status(201).send({ message: "User successfully created" });
+  } catch (e) {
     const { message } = e;
     res.status(400).send({ message });
   }
-}
+};
 
 module.exports = {
-  addUser
-}
+  addUser,
+};
